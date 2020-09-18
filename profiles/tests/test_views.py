9c82aef9eca5edb93d853d03827347c07b88df29
Sathvic Pushpakaran <sathvic.p@gmail.com>
2020-09-18 19:39:12 +0530

    Initial Commit

---
+++ profiles/tests/test_views.py
@@ -0,0 +1,35 @@
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class LoginTest(APITestCase):
+    """
+    Unit tests for User login
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        """
+        Create user in database
+        """
+        cls.user = User.objects.create_user(
+            "testuser", "testuser@test.com", "testingpass"
+        )
+        cls.user.first_name = "John"
+        cls.user.last_name = "Wick"
+        cls.user.save()
+
+    def test_user_names(self):
+        user = User.objects.get_by_natural_key(
+            "testuser"
+        )
+
+        self.assertEqual(user.first_name, self.user.first_name)
+
+    def test_login_post(self):
+        url = reverse('login')
+        login_response = self.client.post(url, {
+            "username": "testuser",
+            "password": "testingpass"
+        })
